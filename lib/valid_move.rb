8d80e0eeaa851f7242cb873d8f309d83f78f95e2
Kate Drennen <k8ted11@gmail.com>
2018-01-15 21:46:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # # code your #valid_move? method here
 def valid_move?(board, index)
-board[index] == " " board[index] == "X" || board[index].nil
+if board[index] == " " board[index] == "X" || board[index].nil
+  false
 end
 
 
